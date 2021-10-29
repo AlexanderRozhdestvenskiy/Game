bae57fa043731358970f330834994b3307e63f30
Alex <alexrozh1988@gmail.com>
2021-10-29 13:24:03 +0300

    Initial Commit

---
+++ Game/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by Alexander Rozhdestvenskiy on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
